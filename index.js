b21b4e19dc03362bc2802d0342418fbf8c425f3f
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-04-01 16:12:36 -0400

    add second js file for sub functions

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { validateCap, validateLow, validateNum, validateSpcl } from './sub-functions'
+
 const validatePassword = (pass) => {
     while (pass.length > 8) {
         return validateCap &&
---
+++ sub-functions.js
@@ -0,0 +1,1 @@
+export { validateCap, validateLow, validateNum, validateSpcl };
