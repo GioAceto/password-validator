63c6250e9544ba1132a154a52e50caaf37d6f2df
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-04-01 15:48:40 -0400

    add while loop to validate password length

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const validatePassword = () => {
-
+const validatePassword = (pass) => {
+    while (pass.length > 8) {
+        return true
+    }
+    return false
 }
 module.exports = validatePassword
