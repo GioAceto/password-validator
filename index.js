d0dbd54d0f1021479ab230b0a46688d18862221b
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-04-01 20:01:33 -0400

    change function and add code to return false if password is missing uppercase letter

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 
 const validatePassword = (pass) => {
+    let result = false
+    let cap = 0
+    let low = 0
     let num = 0
-    if (pass.length > 8) {
-        for (let i = 0; i < pass.length; i++) {
-            if (!isNaN(pass[i])) {
-                num++
+    let spclArray = ['\!']
+    for (let i = 0; i < pass.length; i++) {
+        if (isNaN(pass[i]) && pass[i] === pass[i].toUpperCase()) {
+            for (let count = 0; count < spclArray.length; count++) {
+                if (spclArray[count] !== pass[i]) {
+                    cap++
+                }
             }
+
         }
-        if (num > 0) { return true }
     }
-    return false
+    if ((pass.length > 8) && (cap > 0)) {
+        result = cap
+    }
+    return result
 }
 
+
+
 module.exports = validatePassword
+
+                //if (pass[i] === pass[i].toLowerCase()) {
+            //low++
+            //}
+            //if (!isNaN(pass[i])) {
+            //num++
+            //}
