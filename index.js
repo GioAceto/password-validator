2ae06663486331a95054840f0f39419b29da4728
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-04-01 22:14:51 -0400

    add validation for missing numeric value

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const validatePassword = (pwd) => {
     let cap = 0
     let low = 0
     let num = 0
-    let spclArray = ['\!', '\#', '\$', '\%']
+    let spcl = 0
+    let spclArray = ['\!', '\"', '\#', '\$', '\%', '\&', '\'', '\(', '\)', '\*',]
     for (let i = 0; i < pwd.length; i++) {
         if (isNaN(pwd[i]) && spclArray.indexOf(pwd[i]) === -1) {
             if (pwd[i] === pwd[i].toUpperCase()) {
@@ -12,9 +13,13 @@ const validatePassword = (pwd) => {
             } else if (pwd[i] === pwd[i].toLowerCase()) {
                 low++
             }
+        } else if (!isNaN(pwd[i])) {
+            num++
+        } else if (spclArray.indexOf(pwd[i]) > -1) {
+            spcl++
         }
     }
-    if ((pwd.length > 8) && (cap > 0) && (low > 0)) {
+    if ((pwd.length > 8) && (cap > 0) && (low > 0) && (num > 0) && (spcl > 0)) {
         result = true
     }
     return result
@@ -23,11 +28,3 @@ const validatePassword = (pwd) => {
 
 
 module.exports = validatePassword
-
-                //if (pass[i] === pass[i].toLowerCase()) {
-            //low++
-            //}
-            //if (!isNaN(pass[i])) {
-            //num++
-            //}
-//"\'",'\(',\),\*,\+,\,\-,\.,\/,\:,\;,\<,\=,\>,\?,\@,\[,\,\\,\],\^,\_,\`,\{,\|,\},\~']
