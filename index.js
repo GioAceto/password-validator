2e166e889668708bac09380b5276756c94074680
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-04-04 12:06:03 -0400

    refactored function with additional variables

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
-
 const validatePassword = (pwd) => {
     let result = false
     let cap = 0
     let low = 0
     let num = 0
     let spcl = 0
-    let spclArray = ['\!', '\"', '\#', '\$', '\%', '\&', '\'', '\(', '\)', '\*', '\+', '\-', '\.', '\/', '\:', '\;', '\<', '\=', '\>', '\?', '\@', '\[', '\\', '\]', '\^', '\_', '\`', '\{', '\|', '\}', '\~']
+    let required = 1
+    let spclArray = ['\!', '\"', '\#', '\$', '\%', '\&', '\'', '\(', '\)', '\*', '\+', '\-', '\.', '\/', '\:', '\;', '\<', '\=', '\>', '\?', '\@', '\[', '\\', '\]', '\^', '\_', '\`', '\{', '\|', '\}', '\~', ' ']
+
     for (let i = 0; i < pwd.length; i++) {
-        if (isNaN(pwd[i]) && spclArray.indexOf(pwd[i]) === -1) {
-            if (pwd[i] === pwd[i].toUpperCase()) {
+        let char = pwd[i]
+        if (isNaN(char) && spclArray.indexOf(char) === -1) {
+            if (char === char.toUpperCase()) {
                 cap++
-            } else if (pwd[i] === pwd[i].toLowerCase()) {
+            } else if (char === char.toLowerCase()) {
                 low++
             }
-        } else if (!isNaN(pwd[i])) {
+        } else if (!isNaN(char)) {
             num++
-        } else if (spclArray.indexOf(pwd[i]) > -1) {
+        } else if (spclArray.indexOf(char) > -1) {
             spcl++
         }
+        if ((pwd.length > 8) && ((cap && low && num && spcl) >= required)) {
+            return true
+        }
     }
-    if ((pwd.length > 8) && (cap > 0) && (low > 0) && (num > 0) && (spcl > 0)) {
-        result = true
-    }
+
     return result
 }
-
 
 
 module.exports = validatePassword
