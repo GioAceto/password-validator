a851b467852cbc15d57a09efd42e3d15c569a6ab
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-04-04 15:36:46 -0400

    change function to be modular and invoke sub functions

--- index.js
+++ index.js
@@ -1,31 +1,77 @@
 const validatePassword = (pwd) => {
+    let minLength = 8
     let result = false
+
+    if ((pwd.length >= minLength) && (validateCase(pwd) && validateNum(pwd) && validateSpcl(pwd) === true)) {
+        result = true
+    }
+
+    return result
+}
+
+
+const validateCase = (pwd) => {
+    let spclArray = ['\!', '\"', '\#', '\$', '\%', '\&', '\'', '\(', '\)', '\*', '\+', '\-', '\.', '\/', '\:', '\;', '\<', '\=', '\>', '\?', '\@', '\[', '\\', '\]', '\^', '\_', '\`', '\{', '\|', '\}', '\~', ' ']
     let cap = 0
     let low = 0
-    let num = 0
-    let spcl = 0
+    let caseResult = false
     let required = 1
-    let spclArray = ['\!', '\"', '\#', '\$', '\%', '\&', '\'', '\(', '\)', '\*', '\+', '\-', '\.', '\/', '\:', '\;', '\<', '\=', '\>', '\?', '\@', '\[', '\\', '\]', '\^', '\_', '\`', '\{', '\|', '\}', '\~', ' ']
 
     for (let i = 0; i < pwd.length; i++) {
         let char = pwd[i]
+
         if (isNaN(char) && spclArray.indexOf(char) === -1) {
             if (char === char.toUpperCase()) {
                 cap++
             } else if (char === char.toLowerCase()) {
                 low++
             }
-        } else if (!isNaN(char)) {
-            num++
-        } else if (spclArray.indexOf(char) > -1) {
-            spcl++
-        }
-        if ((pwd.length > 8) && ((cap && low && num && spcl) >= required)) {
-            return true
         }
     }
+    if (cap && low >= required) {
+        caseResult = true
+    }
 
-    return result
+    return caseResult
+}
+
+const validateNum = (pwd) => {
+    let num = 0
+    let numResult = false
+    let required = 1
+
+    for (let i = 0; i < pwd.length; i++) {
+        let char = pwd[i]
+
+        if (!isNaN(char)) {
+            num++
+        }
+    }
+    if (num >= required) {
+        numResult = true
+    }
+
+    return numResult
+}
+
+const validateSpcl = (pwd) => {
+    let spclArray = ['\!', '\"', '\#', '\$', '\%', '\&', '\'', '\(', '\)', '\*', '\+', '\-', '\.', '\/', '\:', '\;', '\<', '\=', '\>', '\?', '\@', '\[', '\\', '\]', '\^', '\_', '\`', '\{', '\|', '\}', '\~', ' ']
+    let spcl = 0
+    let spclResult = false
+    let required = 1
+
+    for (let i = 0; i < pwd.length; i++) {
+        let char = pwd[i]
+
+        if (isNaN(char) && spclArray.indexOf(char) > -1) {
+            spcl++
+        }
+    }
+    if (spcl >= required) {
+        spclResult = true
+    }
+
+    return spclResult
 }
 
 
